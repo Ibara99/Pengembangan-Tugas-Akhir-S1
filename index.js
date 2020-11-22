e5feec40a7ca583d8c25bdcc02dbf2fa4621e66d
ibara <arsip.ibara@gmail.com>
2020-11-22 13:44:20 +0700

    add mqtt connection template

--- index.js
+++ index.js
@@ -1,14 +1,56 @@
-// const dotenv = require('dotenv')
-// const bodyParser = require('body-parser')
+// ini bagian Library-Harus-Pake
+// const dotenv = require('dotenv') //ini buat anu
+// const bodyParser = require('body-parser') //saya lupa ini buat apa awkwk
 const express = require('express')
 const app = express()
 const port = 3000
+
+// ini bagian mqtt
+// var mqtt = require('mqtt');
 
 //config .env
 // const result = dotenv.config()
 // if (result.error) {
 //   throw result.error
 // }
+
+// ini config klo pake EVRYTHNG
+// var thngId=config.thngId; //sepertinya ini username
+// var thngUrl='/thngs/'+thngId; //ini url-topik dasar untuk pub-sub; hanya berlaku untuk evrythng
+// var thngApiKey=config.thngApiKey; // ini password
+// var interval;
+
+// console.log('Using Thng #'+thngId+' with API Key: '+ thngApiKey);
+
+// //koneksi mqtt
+// var client = mqtt.connect("mqtts://mqtt.evrythng.com:8883", {
+//   username: 'authorization',
+//   password: thngApiKey 
+// });
+
+// client.on('connect', () => {  //callback ketika koneksi mqtt berhasil
+//   client.subscribe(thngUrl+'/properties/'); // ini subcribe ke topik apa aja..
+//   updateProperty('livenow', true); // Untuk set property 'livenow', alias ini teh publish
+
+//   if (!interval) interval = setInterval(updateProperties, 5000); // Trigger simulasi read sensor
+// });
+
+// client.on('message', function (topic, message) { // Trigger setiap kali ada update. Dengan kata lain ini teh Subcribe nya
+//   console.log(message.toString());
+//   // harusnya di sini buat nyimpen ke db
+// });
+
+// //in case kamu males nyari lagi, ini ada template buat publish awkwk
+// function updateProperty (property,value) {
+//   client.publish(thngUrl+'/properties/'+property, '[{"value": '+value+'}]');
+// }
+
+// process.on('SIGINT', function() { // Trigger stiap koneksi server (akan) mati
+//   clearInterval(interval); //jangan nanya function ini dapet dari mana awkwk
+//   updateProperty ('livenow', false); //publish status...
+//   client.end();
+//   process.exit();
+// });
 
 //middlewares
 // mount  assets
